f1e07edf4985bded011d9fdb20b38a81c07a2fee
Maryna Karpovets <45387587+maryna-karpovets@users.noreply.github.com>
2020-12-06 14:36:15 +0200

    Task 7. Revert cognito authorization changes (#5)

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,7 +7,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Link } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 
 import { Product } from 'models/Product';
 import { formatAsPrice } from 'utils/utils';
@@ -42,47 +41,17 @@ const useStyles = makeStyles(theme => ({
 export default function Products() {
   const classes = useStyles();
   const [products, setProducts] = useState<Product[]>([]);
-  const { hash } = useLocation();
 
   useEffect(() => {
-    // TODO: remove headers after task 7 is checked
-    const token = hash.split('#id_token=')[1]?.split('&access_token')[0];
-
-    axios
-      .get(`${API_PATHS.product}/products`, {
-        ...(token && {
-          headers: {
-            Authorization: token,
-          },
-        }),
-      })
-      .then(res => setProducts(res.data));
-  }, [hash]);
+    axios.get(`${API_PATHS.product}/products`).then(res => setProducts(res.data));
+  }, []);
 
   if (!products.length) {
     return (
-      <Box component="div" display="block" flex={1} fontWeight="fontWeightBold">
-        <Typography variant="h6" align="center">
-          Please save an id token to get access to the products. Login: test, password: test123.
-        </Typography>
-        <Typography variant="subtitle1" align="center">
-          <Link
-            href="https://user-pool-domain.auth.eu-west-1.amazoncognito.com/login?client_id=18q5ckfo9jfhj9nn41equlkfko&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://d2kdkgfbzg2dbv.cloudfront.net/"
-            target="_blank"
-            style={{ paddingLeft: '10px' }}
-          >
-            Click to login and get an id token
-          </Link>
-        </Typography>
-      </Box>
+      <div className={classes.loader}>
+        <CircularProgress size={50} />
+      </div>
     );
-
-    // TODO: revert after task 7 is checked
-    // return (
-    //   <div className={classes.loader}>
-    //     <CircularProgress size={50} />
-    //   </div>
-    // );
   }
 
   return (
